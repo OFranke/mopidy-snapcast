7942e06debd38c6d94d17bfa534a03b89475021c
Oliver Franke <oliver.franke@aboutyou.de>
2018-04-08 14:18:56 +0200

    entrypoint to autostart snapserver

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,13 @@ then
     export PULSE_COOKIE=$HOME/pulse.cookie
 fi
 
+service=snapserver
+
+if (( $(ps -ef | grep -v grep | grep $service | wc -l) > 0 ))
+then
+echo "$service is running!!!"
+else
+/etc/init.d/$service start
+fi
+
 exec "$@"
